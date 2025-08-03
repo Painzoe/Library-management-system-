2df66226d5ee067bef00ee29ec90e29941097ae9
painzoe <painterzombie16@gmail.com>
2025-08-03 16:29:04 +0300

    Added new features

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Library Management System
 This project is a simple application for organizing your books by genre.
 ## Features 
-1. You can add a book title with its genres and also you can remove then.
-2. You can view all books.
-3. You will be able to view books with their genres.
-4. You can see a menu and also book types for get help.
+1. You can add a book title with its genres.
+2. You can remove a book, a certain type of books or all books in the library.
+3. You can view all books or view a certain type of books.
+4. You can see a menu and also book types for getting help.
 5. If you don't choose exit option, the project will run repeatedly. 
 ## Installation
 You need python program and, then you can copy the code and run. 
--- library.py
+++ library.py
@@ -1,79 +1,95 @@
-book_types = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10
-]
+book_types = {
+    1 : "Fiction" ,
+    2 : "Non-Fiction" ,
+    3 : "Fantasy" ,
+    4 : "Sci-Fi" ,
+    5 : "Mystery/Thriller" ,
+    6 : "Romance" ,
+    7 : "Horror" ,
+    8 : "Biography/Autobiography" ,
+    9 :"Self-Help" ,
+    10 : "Classics"  ,
+    }
+
 book_list = [[] for _ in range(len(book_types))]
 menu = """
  ------ Menu ------
- 1. Add a book
- 2. Remove a book 
- 3. Show all books
+ 1. Add a book 
+ 2. Remove a book / Remove books 
+ 3. View books
  4. Exit
 """
-print(menu)
+
+genres = """
+------ Book Genres ------
+1. Fiction – Made-up stories 
+2. Non-Fiction – Fact-based 
+3. Fantasy – Magic, mythical worlds 
+4. Science Fiction (Sci-Fi) – Futuristic tech, space 
+5. Mystery/Thriller – Crime, suspense, detectives 
+6. Romance – Love stories
+7. Horror – Scary, supernatural 
+8. Biography/Autobiography – Real-life stories 
+9. Self-Help – Personal growth, motivation
+10. Classics – Timeless literature
+"""
 while True:
+    print(menu)
     option = input("Please choose an option: ")
     if option == "1":
-        type= """
-        ------ Book Genres ------
-        1. Fiction – Made-up stories 
-        2. Non-Fiction – Fact-based 
-        3. Fantasy – Magic, mythical worlds 
-        4. Science Fiction (Sci-Fi) – Futuristic tech, space 
-        5. Mystery/Thriller – Crime, suspense, detectives 
-        6. Romance – Love stories
-        7. Horror – Scary, supernatural 
-        8. Biography/Autobiography – Real-life stories 
-        9. Self-Help – Personal growth, motivation
-        10. Classics – Timeless literature
-        """
-        print(type)
+        print(genres)
         book_type = int(input("Select a book type number that suits your book: "))
         book = input("Enter the book name that you want to add: ")
-        book_list += [book_type][book]
+        book_list[book_type - 1].append(book)
         print("Book added!")
 
     elif option == "2":
-        book_to_remove = input("Enter the book name that you want to remove from the library or if you want to remove all books, write \"remove\": ")
-        if book_to_remove == "remove":
-            book_list.clear()
+        remove = """
+        ------ remove options ------
+        1. Remove a book
+        2. Remove all books in a certain type 
+        3. Remove all books in the library
+        """
+        print(remove)
+        choice = int(input("Please choose an option: "))
+        if choice == 1:
+            book_to_remove = input("Enter the book name that you want to remove: ")
+            for i in range(10):
+                if book_to_remove in book_list[i]:
+                    book_list[i].remove(book_to_remove)
+                    print("Book removed!")
+                    break
+            else : print("Book not found!")
+        elif choice == 2:
+            print(genres)
+            genre_to_remove = int(input("Enter the genre number to remove: "))
+            book_list[genre_to_remove - 1].clear()
+        elif choice == 3:
+            for i in range(len(book_list)):
+                book_list[i].clear()
+            print("All books removed!")
         else:
-            if book_to_remove in book_list:
-                book_list.remove(book_to_remove)
-                print("Book removed!")
-            else:
-                print("The book does not exist!")
+            print("Invalid option!")
+
     elif option == "3":
-        if len(book_list) == 0:
-            print("There is no books in the library!")
+        books = """
+        ------ Viewing options ------
+        1.View a certain type of books
+        2.View all books        
+        """
+        print(books)
+        choice = int(input("Please choose an option: "))
+        if choice == 1:
+            print(genres)
+            genre_to_show = int(input("Enter the genre number to show: "))
+            print(book_types[genre_to_show],"Books List:",book_list[genre_to_show - 1])
+        elif choice == 2:
+            for i in range(len(book_list)):
+                print((i +1),")",book_types[i+1],"Books List:",book_list[i])
         else:
-            print("""
-            1. View a certain type of books 
-            2. View all books
-            """)
-            choice = int(input("Please make a choice: "))
-            if choice == 1:
-                book_type = int(input("Which type of book list do you want to view: "))
-                for i in range(len(book_list[book_type])):
-                    print(book_list[book_type][i])
-            elif choice == 2:
-                print("All books in the library: ")
-                for i in range(len(book_list)):
-                    print(i+1,".", book_list[i])
-            else:
-                print("Your choice is not valid!")
+            print("Invalid option!")
 
     elif option == "4":
         exit()
     else :
         print("Invalid option!")
-
-
